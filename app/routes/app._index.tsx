d6bb01e5eb0f3b120804d285cb530b21b1e77a3d
Alon <kiglerr@gmail.com>
2025-06-03 20:14:49 +0300

    🎉

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -124,7 +124,7 @@ export default function Index() {
               <BlockStack gap="500">
                 <BlockStack gap="200">
                   <Text as="h2" variant="headingMd">
-                    Congrats on creating a new Shopify app 🎉
+                    🎉🎉🎉🎉🎉🎉🎉🎉🎉
                   </Text>
                   <Text variant="bodyMd" as="p">
                     This embedded app template uses{" "}
