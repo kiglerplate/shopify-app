5fdc66b73889ab32f1249c982218a73fd7036eda
Alon Kigler <kiglerr@gmail.com>
2025-06-04 21:04:07 +0300

    וובהוקים בייבי

--- app/routes/webhooks.app.uninstalled.tsx
+++ app/routes/webhooks.app.uninstalled.tsx
@@ -1,21 +1,12 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
-import db from "../db.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const { shop, session, topic } = await authenticate.webhook(request);
+  const { shop, topic, payload } = await authenticate.webhook(request);
 
-  console.log("🔔 Webhook Received:");
-  console.log("Shop Domain:", shop);
-  console.log("Topic:", topic);
-
-  const payload = await request.json();
-  console.log("Payload:", payload);
-
-  // מחיקת הסשנים מה-DB במקרה של uninstall
-  if (session) {
-    await db.session.deleteMany({ where: { shop } });
-  }
+  console.log("✅ Webhook Received:", topic);
+  console.log("🛒 Shop:", shop);
+  console.log("📦 Payload:", payload);
 
   return new Response("ok");
 };
